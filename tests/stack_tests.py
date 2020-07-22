964584a69be37b7fc0c35c0f3fa8d2013597c9c1
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2020-07-22 21:55:41 +0100

    Fixed tests

--- tests/stack_tests.py
+++ tests/stack_tests.py
@@ -5,7 +5,9 @@ import torchdiffeq
 
 from problems import construct_problem
 
-error_tol = 1e-2
+error_tol = 1e-3
+error_atol = 1e-3
+error_rtol = 1e-3
 
 torch.set_default_dtype(torch.float64)
 devices = ['cpu']
@@ -41,6 +43,12 @@ def rel_error(true, estimate):
 
 class TestStacked(unittest.TestCase):
 
+    def allclose(self, tensor1, tensor2, rtol, atol):
+        # Not using torch.allclose so that we get explicit printout of what the error and tol are.
+        error = tensor1.sub(tensor2).abs().max()
+        tol = atol + rtol * tensor2.abs().max()
+        self.assertLess(error, tol)
+
     def test_forward(self):
         for device in devices:
             for ode in ('constant', 'linear', 'sine'):
@@ -63,6 +71,7 @@ class TestStacked(unittest.TestCase):
                     y = torchdiffeq.odeint(f, y0, t_points, rtol=1e-7, atol=1e-12)
                     k = torch.rand_like(y)
                     y.backward(k)
+
                     y0_grad = y0.grad.clone()
                     t_points_grad = t_points.grad.clone()
                     param_grads = tuple(param.grad.clone() for param in f.parameters())
@@ -80,13 +89,11 @@ class TestStacked(unittest.TestCase):
 
                         y0_grad_ = y0.grad
                         t_points_grad_ = t_points.grad
-                        self.assertLess(rel_error(y0_grad, y0_grad_), error_tol)
-                        # For some reason this can get quite large errors.
-                        # Not sure why.
-                        self.assertLess(rel_error(t_points_grad, t_points_grad_), 10 * error_tol)
+                        self.allclose(y0_grad_, y0_grad, error_rtol, error_atol)
+                        self.allclose(t_points_grad_, t_points_grad, error_rtol, error_atol)
                         for param, param_grad in zip(f.parameters(), param_grads):
                             param_grad_ = param.grad
-                            self.assertLess(rel_error(param_grad, param_grad_), error_tol)
+                            self.allclose(param_grad_, param_grad, error_rtol, error_atol)
 
                         y0.grad.zero_()
                         t_points.grad.zero_()
